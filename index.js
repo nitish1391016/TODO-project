5f6ea4385fc75a7f0ec34c468d21f06dc8c87fb4
nitish <nitishkumar1391016@gmail.com>
2023-08-23 15:59:01 +0530

    change stying and set duplicate data entering while refresh

--- index.js
+++ index.js
@@ -15,62 +15,65 @@ var dailyRunning=true;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const list1 = [];
-const list2 = [];
+const list1 = [
+  "Morning exercise",
+  "Breakfast",
+  "Check emails",
+  "Attend team meeting",
+  "Work on project A",
+  "Lunch break",
+  "Review project B",
+  "Call with client",
+  "Submit weekly report",
+  "Read for 30 minutes",
+  "Cook dinner",
+  "Relax and unwind",
+  "Plan tomorrow's tasks"
+];
+const list2 = [
+  "Prepare presentation for meeting",
+  "Review project timeline",
+  "Client follow-up emails",
+  "Brainstorm new marketing ideas",
+  "Code review for feature X",
+  "Submit expense report",
+  "Update project documentation",
+  "Meet with cross-functional team",
+  "Research industry trends",
+  "Design UI mockups",
+  "Collaborate on project proposal",
+  "Write blog post for company website",
+  "Organize files and folders",
+  "Conduct user testing",
+  "Schedule social media posts"
+];
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 let list=[];
 app.get("/", (req, res) => {
-  if(dailyRunning){
-    list=list1;
-    res.render("index.ejs",{
-      list
-    });
-  }
-  else{
-    list=list2;
-    res.render("index.ejs",{
-      list
-    });
-  }
+  list=dailyRunning ? list1: list2;
+  res.render("index.ejs",{
+    list
+  });
 });
 
 app.post("/index",(req,res)=>{  
   dailyRunning=true;
-  if(dailyRunning){
-    list=list1
-    res.render("index.ejs",{
-      list
-    });
-  }
-  else{
-    list=list2
-    res.render("index.ejs",{
-      list
-    });
-  }
+  list=dailyRunning ? list1 : list2;
+  res.redirect("/");
 });
 
 
 app.post("/submit",(req,res)=>{ 
   var ele=req.body.input_value;
-  if(ele!="" && dailyRunning==true){
-    list1.push(ele);
-    list=list1;
-    res.render("index.ejs",{
-      list
-    });
+  list= dailyRunning ? list1 : list2;
+  if(ele!=""){
+    list.push(ele);
   }
-  else if(ele!="") {
-    list2.push(ele);
-    list=list2;
-    res.render("index.ejs",{
-      list
-    });
-  }
-  
+  res.redirect("/");
 });
 
 app.post("/click",(req,res)=>{
